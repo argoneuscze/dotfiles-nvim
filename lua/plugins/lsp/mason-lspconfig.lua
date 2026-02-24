4c24164a2db5f462a9f2f07a80aaa52b79f4a902
argoneus <argoneuscze@gmail.com>
2026-02-24 17:52:15 +0100

    added conform formatting

--- lua/plugins/lsp/mason-lspconfig.lua
+++ lua/plugins/lsp/mason-lspconfig.lua
@@ -1,26 +1,31 @@
 local servers = {
-    "lua_ls", "pyright"
+	-- LSPs
+	"lua_ls",
+	"pyright",
+	-- Formatters
+	"stylua",
+	-- Linters
+	"ruff",
 }
 
 return {
-    "mason-org/mason-lspconfig.nvim",
-    dependencies = {
-        "mason-org/mason.nvim",
-        "neovim/nvim-lspconfig"
-    },
-    config = function()
-        require("mason-lspconfig").setup({
-            ensure_installed = servers,
-            automatic_enable = false
-        })
+	"mason-org/mason-lspconfig.nvim",
+	dependencies = {
+		"mason-org/mason.nvim",
+		"neovim/nvim-lspconfig",
+	},
+	config = function()
+		require("mason-lspconfig").setup({
+			ensure_installed = servers,
+			automatic_enable = false,
+		})
 
-        local capabilities = require('cmp_nvim_lsp').default_capabilities()
-        for _, server in ipairs(servers) do
-            vim.lsp.config(server, {
-                capabilities = capabilities
-            })
-            vim.lsp.enable(server)
-        end
-    end
+		local capabilities = require("cmp_nvim_lsp").default_capabilities()
+		for _, server in ipairs(servers) do
+			vim.lsp.config(server, {
+				capabilities = capabilities,
+			})
+			vim.lsp.enable(server)
+		end
+	end,
 }
-
