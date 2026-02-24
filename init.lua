0bb9fad214ca142f3a4256052b19a59f8490b1cd
argoneus <argoneuscze@gmail.com>
2026-02-24 01:13:38 +0100

    initial

---
+++ init.lua
@@ -0,0 +1,26 @@
+-- basics    
+vim.o.number = true
+vim.g.have_nerd_font = true
+vim.o.number = true
+vim.o.mouse = 'a'
+vim.o.showmode = false
+vim.o.confirm = true
+
+-- search
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- tab
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+
+-- set leader to <space>
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- plugins
+require "lazynvim"
+require "theme"
+
