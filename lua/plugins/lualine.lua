49002daf9d44426d3cda7cdcae757ecfb4432190
argoneus <argoneuscze@gmail.com>
2026-03-02 18:13:43 +0100

    oil and lualine show path

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,5 +1,14 @@
 return {
   "nvim-lualine/lualine.nvim",
   dependencies = { "echasnovski/mini.icons" },
-  opts = {},
+  opts = {
+    sections = {
+      lualine_c = {
+        {
+          "filename",
+          path = 1,
+        },
+      },
+    },
+  },
 }
