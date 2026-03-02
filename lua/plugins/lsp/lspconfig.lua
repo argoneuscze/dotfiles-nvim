395b9a644c0f0de8323108b6d43235b9c1dcc32d
argoneus <argoneuscze@gmail.com>
2026-03-02 19:43:33 +0100

    fixed basedpyright config

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -4,11 +4,9 @@ return {
     -- Python
     vim.lsp.config("basedpyright", {
       settings = {
-        pyright = {
+        basedpyright = {
           -- Using Ruff's import organizer
           disableOrganizeImports = true,
-        },
-        python = {
           analysis = {
             -- Ignore all files for analysis to exclusively use Ruff for linting
             ignore = { "*" },
